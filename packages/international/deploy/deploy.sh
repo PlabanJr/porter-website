56e7cbae533ff7bce95d0d79e4e7b4a8493194b5
ayazsaif <mohd.ayaz@theporter.in>
2024-03-19 12:13:40 +0530

    deploy

--- packages/international/deploy/deploy.sh
+++ packages/international/deploy/deploy.sh
@@ -6,13 +6,23 @@ set -e
 
 ENV=$1
 BOX=$2
+INFRA_STACKS=$3
 CURR_TS=$(date +"%s")
 ENV_FILE=.env
 
+if [ "$ENV" == "staging" ]; then
+  if [ -z "$OMS_URL" ]; then
+    print_usage
+    exit 1
+  fi
+fi
 
 #prepare environment variables
 echo "RELEASE_TS=$CURR_TS" >>$ENV_FILE
 
+if [ ! -z "$INFRA_STACKS" ]; then
+  echo "INFRA_STACKS=$INFRA_STACKS" >>$ENV_FILE
+fi
 
 source ./$ENV_FILE
 
