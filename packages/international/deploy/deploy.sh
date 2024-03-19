1391c78e5c4b31ee038857817a02cd71d2bcbf73
ayazsaif <mohd.ayaz@theporter.in>
2024-03-19 12:38:24 +0530

    deploy

--- packages/international/deploy/deploy.sh
+++ packages/international/deploy/deploy.sh
@@ -10,7 +10,7 @@ INFRA_STACKS=$3
 CURR_TS=$(date +"%s")
 ENV_FILE=.env
 
-aws s3 sync s3://porter-backend-platforms/packages/deployer/v1/ packages/international/deploy/
+aws s3 sync s3://porter-backend-platforms/packages/deployer/v1/ packages/international/deploy/deployer/
 
 #prepare environment variables
 echo "RELEASE_TS=$CURR_TS" >>$ENV_FILE
